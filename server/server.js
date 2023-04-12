40409aa7d8fd5bc0a692b69cd747e191e1d0f1c7
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2023-04-12 13:23:25 -0400

    all

--- server/server.js
+++ server/server.js
@@ -1,34 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const multer = require('multer');
 const app = express();
+
+// Serve static files from the public directory
+
+app.use(express.static('server', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.js')) {
+      res.set('Content-Type', 'text/javascript');
+    } else if (path.endsWith('.html')) {
+      res.set('Content-Type', 'text/html');
+    }
+  }
+}));
+
+app.get('/client.js', function(req, res) {
+  res.set('Content-Type', 'text/javascript');
+  res.sendFile(__dirname + '/client.js');
+});
+
+// Route for handling GET requests to the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 // Use body-parser to parse JSON data in request body
 app.use(bodyParser.json());
 
+// Set up multer storage configuration
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 // Route for handling POST request to /sendemail
-app.post('/sendemail', (req, res) => {
-  const dataURL = req.body.dataURL;
+app.post('/sendemail', upload.single('photo'), (req, res) => {
+  const photoBuffer = req.file.buffer;
 
   // Use nodemailer to send email with photo attachment
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'Zoho',
     auth: {
-      user: 'bagapp69@gmail.com',
-      pass: 'bagapp123'
+      user: 'bagapp@zohomail.com',
+      pass: '1drHPSFMUPVh'
     }
   });
 
   const mailOptions = {
-    from: 'bagapp69@gmail.com',
+    from: 'bagapp@zohomail.com',
     to: 'bagapp69@gmail.com',
     subject: 'Captured Photo',
     text: 'Photo attached.',
     attachments: [
       {
         filename: 'photo.jpg',
-        content: dataURL.split(';base64,').pop(),
-        encoding: 'base64'
+        content: photoBuffer
       }
     ]
   };
@@ -36,10 +62,10 @@ app.post('/sendemail', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error sending email', error);
-      res.sendStatus(500);
+      res.status(500).json({ success: false, error: 'Error sending email' });
     } else {
       console.log('Email sent successfully', info.response);
-      res.sendStatus(200);
+      res.status(200).json({ success: true });
     }
   });
 });
